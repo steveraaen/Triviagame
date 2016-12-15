fedebaa3570eedf595bfb0d7b4039c247a660934
steveraaen <sraaen@gmail.com>
2016-12-15 13:37:26 -0500

    new timers fewer apps

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 var numCorrect = 0,
     numIncorrect = 0,
     unanswered = 0,
-    questionTimer = 10,
-    intervalTimer = 5;
+    questionTimer = 12;
+
 
 var q1 = {
     'question': 'Which was the first and only racehorse to defeat Man O War?',
@@ -10,7 +10,7 @@ var q1 = {
     'answers': ['Secretariat', 'Seabiscuit', 'Upset', 'Mister Ed', 'Affirmed'],
     'media': '<img id = "ap" src ="./assets/images/manowar.gif">',
     'explainer': 'It was at Saratoga, in 1919, that the word “upset” entered the American sports lexicon. That\'s when a horse named Upset beat the mighty Man o\' War. It was the original Big Red\'s only defeat. In those days, the word upset had a more literal meaning, along the lines of tip over, or capsize.'
-};
+}
 var q2 = {
     'question': 'Which of these cities is east of Reno, NV?',
     'answer': 'Los Angeles\, CA',
@@ -25,26 +25,79 @@ var q3 = {
     'media': '<img id = "ap" src ="./assets/images/champagne.gif">',
     'explainer': 'Pérignon was born to a clerk of the local marshal in the town of Sainte-Menehould in the ancient Province of Champagne in the Kingdom of France. He was born in December 1638 and was baptized on 5 January 1639. He was the youngest of his parents\' seven children, as his mother died the following summer. His father\'s family owned several vineyards in the region.'
 }
+var q4 = {
+    'question': 'Texas state law requires that you do not do this to your neighbor\'s cow?',
+    'answer': 'Draw graffiti',
+    'answers': ['Discuss Politics', 'Talk dirty', 'Draw graffiti', 'Talk about religion', 'Barbecue'],
+    'media': '<img id = "ap" src ="./assets/images/cow.jpg">',
+    'explainer': 'ly owned several vineyards in the region.'
+}
+var q5 = {
+    'question': 'Who was Uncle Sam\'s close childhood friend?',
+    'answer': 'Johnny Appleseed',
+    'answers': ['', 'Alexander Hamilton', 'Johnny Appleseed', 'George Washington', 'Benjamin Franklin'],
+    'media': '<img id = "ap" src ="./assets/images/japple.jpg">',
+    'explainer': 'Pérignon was born to a clerk of the local marshal in the town of Sainte-Menehould in the ancient Province of Champagne in the Kingdom of France. He was born in December 1638 and was baptized on 5 January 1639. He was the youngest of his parents\' seven children, as his mother died the following summer. His father\'s family owned several vineyards in the region.'
+}
+var q6 = {
+    'question': 'Who was the legendary Benedictine monk who invented champagne?',
+    'answer': 'Dom Perignon',
+    'answers': ['Roland Champagne', 'Sebastien Clicquot', 'Dom Perignon', 'Henri Moet', 'Benjamin Franklin'],
+    'media': '<img id = "ap" src ="./assets/images/champagne.gif">',
+    'explainer': 'Pérignon was born to a clerk of the local marshal in the town of Sainte-Menehould in the ancient Province of Champagne in the Kingdom of France. He was born in December 1638 and was baptized on 5 January 1639. He was the youngest of his parents\' seven children, as his mother died the following summer. His father\'s family owned several vineyards in the region.'
+}
 var qa = [q1, q2, q3];
-var curQues;
+var curQues = {};
 var choice;
 var pickQ;
+var questionTimer;
+
 $(document).ready(function() {
-    function firstQ() {
-        pickQ = Math.floor(Math.random() * (qa.length));
-        for (let i = 0; i < qa.length; i++) {
-            curQues = qa[pickQ];
-            $('#question').html(curQues.question);
+   function setTimer(){
+    setInterval(timer, 1000);
+    questionTimer = 12;
+    function timer () {    
+        questionTimer--;
+        $('#timer').html(questionTimer)
+        console.log(questionTimer);
+        if (questionTimer <= 0) {
+            timesUp()
+            stopInterval()
         }
-        for (let i = 0; i < curQues.answers.length; i++) {
-            $('#choices').append('<div class = "answers">' + curQues.answers[i] + '</div>');
-        };
-        
+        }
     }
-    firstQ();
+    setTimer()
+function stopInterval(){
+for( let i=0; i<10000; i++)     {
+        window.clearInterval(i);
+    }
+}           
+    $('div').on('click', '.answers', function() {
+        choice = $(this).text();
+        if (curQues.answer === choice) {
+            stopInterval()
+            $('#media').html(curQues.media);
+            $('#message').text("Correct!");
+            numCorrect++;
+            clickSet = setTimeout(nextQ, 4000)     
+        } else {
+            $('#message').text("Wrong.  Try Again");
+            numIncorrect++;
+        }
+    });
+    function timesUp() {
+        stopInterval()
+        $('#media').html(curQues.media)
+        $('#message').text("Times Up!  The answer was " + curQues.answer + "!");
+        unanswered++;
+        setTimeout(nextQ, 4000);
+    }
     function nextQ() {
-        $('#media').empty();
-        $('#question').empty();
+/*        clearTimeout(nextQ);*/
+        stopInterval();
+        questionTimer = 12;
+        setTimer()
+        $('#timer').html(questionTimer);
         $('#choices').empty();
         pickQ = Math.floor(Math.random() * (qa.length));
         for (let i = 0; i < qa.length; i++) {
@@ -55,32 +108,6 @@ $(document).ready(function() {
             $('#choices').append('<div class = "answers">' + curQues.answers[i] + '</div>');
         };
 
-    }
-function timesUp(){
-		    unanswered++;
-            $('#message').text("Times Up!  The answer was " + curQues.answer + "!");
-            $('#media').html(curQues.media);
-            setTimeout(nextQ, 3000);
-/*            clearInterval(counter);*/
-}
-    $('div').on('click', '.answers', function() {
-        clearTimeout(delay)
-        choice = $(this).text();
-        if (curQues.answer === choice) {
-
-/*        	$('#timer').html(counter)*/
-            $('#media').html(curQues.media)
-            $('#message').text("Correct!");
-            numCorrect++;
-            var delay = setTimeout(nextQ, 3000);
-/*        	clearInterval(counter);*/
-            
-        }else {	
-/*        	$('#timer').html(counter)*/
-            $('#message').text("Wrong.  Try Again");
-            numIncorrect++;
-/*            clearInterval(counter);*/
         }
-
-    });
+nextQ()
 });
